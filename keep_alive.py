a1c5d32ba025cf37b0587f638d5739ade53fa45d
Ryan Blocker <62674519+ryancblocker@users.noreply.github.com>
2024-04-30 11:42:07 -0600

    keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,7 @@
+import pyautogui
+import time
+
+while True:
+    pyautogui.click()  # Clicks at the current mouse location.
+    time.sleep(10)  # Waits for 60 seconds. Adjust the number as needed.
+
